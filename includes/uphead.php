f22082109c3c1d2a7159ccc1f10903d61c3a0c8d
Emmanuel Kwaku Botchway <emmlquake@live.com>
2022-02-01 13:58:23 +0000

    Updates to footer

--- includes/uphead.php
+++ includes/uphead.php
@@ -2,23 +2,23 @@
 <div class="up-head d-none d-lg-block background-grey-4">
     <div class="container">
         <div class="row">
-            <div class="col-xl-10 col-lg-12">
+            <div class="col-xl-8 col-lg-12">
                 <div class="row">
-                    <div class="col-md-6">
+                    <div class="col-md-4">
                         <i class="fa fa-phone margin-right-10px"></i>
                         650-876-2022
                     </div>
-                    <div class="col-md-6">
+                    <div class="col-md-5">
                         <i class="fa fa-envelope-o margin-right-10px"></i>
                         info@inlandlogisticshandlers.com
                     </div>
-                    <div class="col-md-6">
+                    <div class="col-md-5">
                         <i class="fa fa-map-marker margin-right-10px"></i>
                         3 Canal Plaza, GF Portland
                     </div>
                 </div>
             </div>
-            <div class="col-xl-2 d-none d-xl-block">
+            <div class="col-xl-4 d-none d-xl-block">
                 <div class="row">
                     <div class="col-lg-6">
                         <?php include('social.php') ?>
